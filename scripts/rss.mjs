ecb642ad6bdb0501300cc474039a6e0063451e96
wordopera <wordopera@gmail.com>
2024-10-18 19:28:45 -0400

    push it 2

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -5,7 +5,8 @@
 import { writeFileSync, mkdirSync } from 'fs';
 import path from 'path';
 import { slug } from 'github-slugger';
-import { escape } from 'pliny/utils/htmlEscaper';
+// import { escape } from 'pliny/utils/htmlEscaper';
+import { encode as escape } from 'he';
 import siteMetadata from '../data/siteMetadata';
 import tagData from '../app/tag-data.json' assert { type: 'json' };
 import { allBlogs } from '../.contentlayer/generated/index';
