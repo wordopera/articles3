51ce99f394bccc1d8f4e2367a6586e9ff3dda782
wordopera <wordopera@gmail.com>
2024-10-18 00:11:13 -0400

    inprof bus cases added

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,7 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: 'Clients & Results' },
+
+  { href: '/projects', title: 'Articles' },
 ]
 
 export default headerNavLinks
